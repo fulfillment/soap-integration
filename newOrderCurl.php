1f2af5a76c2c6263e6248509cf70a75f9d381980
Mike Garde <mgarde@fulfillment.com>
2017-02-17 09:31:45 -0500

    Initial Commit
    Simple example

---
+++ newOrderCurl.php
@@ -0,0 +1,148 @@
+<?php
+
+/*
+ * Sample order data
+ */
+
+$clientDetails  = [
+	'merchant_id' => '50001',
+	'api_key'     => 'ABC123',
+];
+$orderDetails   = [
+	'order_id'        => date('U'),
+	'campaign_id'     => '',
+	'ship_group_code' => 'Track,Cheapest,Fastest',
+	'ship_fname'      => 'test',
+	'ship_lname'      => 'test',
+	'ship_address1'   => '1600 Pennsylvania Ave NW',
+	'ship_address2'   => '',
+	'ship_city'       => 'Washington',
+	'ship_state'      => 'DC',
+	'ship_zip'        => '20500',
+	'ship_country'    => 'US', # Please use 2 letter ISO 3166-1
+	'ship_phone'      => '8005551212', # Phone & Email are required for international packages
+	'ship_email'      => 'sales@fulfillment.com',
+	'Comments'        => '',
+];
+$productDetails = [
+	[
+		'sku'      => 'sku1',
+		'quantity' => 1,
+		'price'    => 3.99,
+	],
+	[
+		'sku'      => 'sku2',
+		'quantity' => 1,
+		'price'    => 3.99,
+	],
+];
+
+/*
+ * Build our XML request
+ */
+
+$productXml = '';
+
+$i = 1;
+foreach ($productDetails as $productDetail)
+{
+	$noun   = ($i === 1) ? 'product' : 'upsell';
+	$format = '<%1$s_%2$d_sku>%3$s</%1$s_%2$d_sku><%1$s_%2$d_quantity>%4$d</%1$s_%2$d_quantity><%1$s_%2$d_price>%5$s</%1$s_%2$d_price>';
+
+	$productXml .= sprintf($format, $noun, $i, $productDetail['sku'], $productDetail['quantity'], $productDetail['price']);
+	$i++;
+}
+unset($noun, $i, $format, $productDetail);
+
+$xmlRequest = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope>
+    <SOAP-ENV:Body>
+        <tns:execute>
+            <person>
+                <merchant_id>{$clientDetails['merchant_id']}</merchant_id>
+                <api_key>{$clientDetails['api_key']}</api_key>
+                <campaign_id>{$orderDetails['campaign_id']}</campaign_id>
+                <order_id>{$orderDetails['order_id']}</order_id>
+                <ship_group_code>Trackable,Cheapest,Fastest</ship_group_code>
+                <ship_fname>{$orderDetails['ship_fname']}</ship_fname>
+                <ship_lname>{$orderDetails['ship_lname']}</ship_lname>
+                <ship_address1>{$orderDetails['ship_address1']}</ship_address1>
+                <ship_address2>{$orderDetails['ship_address2']}</ship_address2>
+                <ship_city>{$orderDetails['ship_city']}</ship_city>
+                <ship_state>{$orderDetails['ship_state']}</ship_state>
+                <ship_zip>{$orderDetails['ship_zip']}</ship_zip>
+                <ship_country>{$orderDetails['ship_country']}</ship_country>
+                <ship_phone>{$orderDetails['ship_phone']}</ship_phone>
+                <ship_email>{$orderDetails['ship_email']}</ship_email>
+                <Comments></Comments>
+                {$productXml}
+            </person>
+        </tns:execute>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>";
+
+/*
+ * Submit Order to Fulfillment.com
+ */
+try
+{
+	$curl = curl_init();
+
+	curl_setopt_array($curl, [
+		CURLOPT_URL            => 'https://app.atcostfulfillment.com/api/order',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING       => "",
+		CURLOPT_MAXREDIRS      => 10,
+		CURLOPT_TIMEOUT        => 30,
+		CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST  => 'POST',
+		CURLOPT_POSTFIELDS     => $xmlRequest,
+		CURLOPT_HTTPHEADER     => [
+			'cache-control: no-cache',
+			'content-type: text/xml',
+			'soapaction: urn:order#execute',
+			'user-agent: crmName',
+		],
+	]);
+
+	$fdcResponse  = curl_exec($curl);
+	$fdcCurlError = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($fdcCurlError)
+	{
+		throw new Exception("cURL Error #:$fdcCurlError");
+	}
+
+	/*
+	 * You have a response from Fulfillment.com
+	 * Parse it and decide how to move forward
+	 */
+
+	$doc = new DOMDocument('1.0', 'utf-8');
+	$doc->loadXML($fdcResponse);
+
+	$fdcResult = $doc->getElementsByTagName("status");
+	$fdcResult = $fdcResult->item(0)->nodeValue;
+
+	$fdcResultDesc = $doc->getElementsByTagName("error_desc");
+	$fdcResultDesc = $fdcResultDesc->item(0)->nodeValue;
+
+	if ($fdcResult == 'Success' || $fdcResultDesc == 'Duplicate order!')
+	{
+		// Fulfillment.com has received your order
+		// If you want to record our order ID you can however you can ask us about your order using your order ID
+		$fdcOrderId = $doc->getElementsByTagName("acf_id");
+		$fdcOrderId = (int) $fdcOrderId->item(0)->nodeValue;
+	}
+	else
+	{
+		$errorMsg = 'FDC responded with the following error "%s" / "%s"';
+		throw new Exception(sprintf($errorMsg, $fdcResult, $fdcResultDesc));
+	}
+
+}
+catch (Exception $e)
+{
+	// do something
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Fulfillment.com SOAP API
+
+Four years ago FDC launched support for receiving orders, getting tracking information, 
+canceling an order, and retrieving returned orders via a SOAP API. Details can be found
+here, [Integration Guide](https://www.fulfillment.com/integration-guide/). We have since
+developed a REST-ful API and will be working with our clients and integrated partners
+over the next few months (early to mid 2017) to finalize standards.
+
+At this point (February 2017) we encourage new integrations to call their account
+executive for additional details on our [REST API](https://api.fulfillment.com/docs/api).
+
+## NuSOAP
+
+Our [Integration Guide](https://www.fulfillment.com/integration-guide/) outlines the fastest way to get started.
+However NuSOAP is no longer being supported and carries with it unnecessary GETs as we will not be changing our
+API setup.
+
+## SOAP Integration Without NuSOAP
+
+Use [newOrderCurl.php](https://github.com/Fulfillment-dot-com/soap-intergration/blob/master/newOrderCurl.php)
+as an example. I believe most developers will prefer this version.
